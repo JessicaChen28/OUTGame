d0442fc7e889410da608a93392952251bf385229
JessicaChen28 <123920453+JessicaChen28@users.noreply.github.com>
2023-05-11 19:21:06 +0000

    add images

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
-function setup(){
-  createCanvas(400,400);
-}
-function draw(){
-  background(220);
-  ellipse(50,50,50,50);
-}
-console.log("Hello");
-console.log("Goodbye");
-
-console.log("Hi");
-
 const back1 = new Image();
 back1.addEventListener (
   "load", () => {
-    var c = document.getElementById("myCanvas");
+  var c = document.getElementById("myCanvas");
   var ctx = c.getContext("2d");
   ctx.drawImage(back1,0 ,0);
-    c.addEventListener("keydown", (e) => {
+  c.addEventListener("keydown", (e) => {
   if (!e.repeat) {
     console.log(`Key "${e.key}" pressed [event: keydown]`);
   } else {
@@ -28,31 +16,24 @@ back1.addEventListener (
 )
 back1.src = "images/campdrawingbackground1.jpg";
 
-// const back2 = new Image();
-// back2.addEventListener(
-//   "load", () => {
-    
-//   }
-// )
-
-// c.addEventListener("keydown", (e) => {
-//   if (!e.repeat) {
-//     logMessage(`Key "${e.key}" pressed [event: keydown]`);
-//   } else {
-//     logMessage(`Key "${e.key}" repeating [event: keydown]`);
-//   }
-// });
-
+const blackTrans = new Image();
+blackTrans.onkeypress =  function(){};
+blackTrans.addEventListener(
+  "keypress", () => {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.drawImage(blackTrans, 0, 0);
+  }
+)
+blackTrans.src = "images/black.jpg";
 
 
 //A variable container
 //Declaration var, let, const
 
-let name = "Amy"; //string
-let isRainy = false; //boolean
-let age = 21; //number
+// let name = "Amy"; //string
+// let isRainy = false; //boolean
+// let age = 21; //number
 
-console.log(name);
-console.log("is it rainy " + isRainy);
-
-document.getElementById("p1").innerHTML = "Hello " + name;
+// console.log(name);
+// console.log("is it rainy " + isRainy);
